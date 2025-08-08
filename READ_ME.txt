ec2faf54039393ddbcb3dd7dd6bde9518796e49f
Alex Righi <alex.righi@outlook.it>
2025-08-08 14:13:37 +0200

    Initial commit

---
+++ READ_ME.txt
@@ -0,0 +1,8 @@
+Per eseguire il programma:
+- pip install -r requirements.txt
+- python3 estrai_classifica.py
+
+Per vedere su index.html i risultati dell'esecuzione:
+- python3 -m http.server 8080
+- python3 server.py
+- Apro nel browser: http://127.0.0.1:8080
---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import subprocess
+import json
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/esegui', methods=['GET'])
+def esegui_script():
+
+    try:
+        # Esegue lo script Python
+        subprocess.run(['python3', 'estrai_classifica.py'], check=True)
+
+        # Controlla che il file squadre.json esista
+        if not os.path.exists('squadre.json'):
+            return jsonify({'errore': 'File squadre.json non trovato'}), 500
+
+        # Legge e ritorna il contenuto del JSON
+        with open('squadre.json', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        return jsonify(data)
+
+    except subprocess.CalledProcessError:
+        return jsonify({'errore': 'Errore nell\'esecuzione di estrai_classifica.py'}), 500
+
+    except Exception as e:
+        return jsonify({'errore': str(e)}), 500
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
