607018890d7250dbb61a975413a8691c02a4b234
Alex Righi <alex.righi@outlook.it>
2025-08-14 03:09:42 +0200

    Navbar responsive ma non completa

--- front-end/src/components/Navbar_FFC.jsx
+++ front-end/src/components/Navbar_FFC.jsx
@@ -1,15 +1,48 @@
+import { useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import styles from "./Navbar_FFC.module.css";
 
 const Navbar_FFC = () => {
+
+  let isMenuOpen = false;
+  const clickMenu = () => {
+    isMenuOpen = !isMenuOpen;
+    document.querySelector(".sideMenu").style.transform = isMenuOpen ? "translateX(0)" : "translateX(100%)";
+  };
+
+  const closeMenu = () => {
+    isMenuOpen = false;
+    document.querySelector(".sideMenu").style.transform = "translateX(100%)";
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 992) {
+        // chiudi automaticamente il menu
+        document.querySelector(".sideMenu").style.transform = "translateX(100%)";
+        isMenuOpen = false;
+      }
+    };
+
+    // Aggiungo listener
+    window.addEventListener("resize", handleResize);
+
+    // Pulizia del listener
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+
+
   return (
     <Navbar expand="lg" className={`${styles.navbar} sticky-top`} variant="dark">
-      <Container fluid className={styles.navContainer}>
+      <Container fluid className={`${styles.navContainer} blurred`}>
 
         <Nav className={`${styles.navMenu}`}>
-          <Nav.Link href="#home" className={styles.link}>HOME</Nav.Link>
-          <Nav.Link href="#calendario" className={styles.link}>CALENDARIO</Nav.Link>
+            <Nav.Link href="#home" className={styles.link}>HOME</Nav.Link>
+            <Nav.Link href="#calendario" className={styles.link}>CALENDARIO</Nav.Link>
         </Nav>
 
         <Navbar.Brand href="#">
@@ -17,9 +50,31 @@ const Navbar_FFC = () => {
         </Navbar.Brand>
 
         <Nav className={`${styles.navMenu}`}>
-          <Nav.Link href="#rosa" className={styles.link}>ROSA</Nav.Link>
-          <Nav.Link href="#chi-siamo" className={styles.link}>CHI SIAMO</Nav.Link>
+            <Nav.Link href="#rosa" className={styles.link}>ROSA</Nav.Link>
+            <Nav.Link href="#chi-siamo" className={styles.link}>CHI SIAMO</Nav.Link>
         </Nav>
+
+        <Nav className={styles.navTendina}>
+          <div className={styles.hamburger} onClick={clickMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>         
+        </Nav>
+
+
+        <div className="sideMenu">
+          <div className={styles.closeIcon} onClick={closeMenu}>
+            <i className="bi bi-x-lg"></i>
+          </div>
+          <Nav className={styles.sideNav}>
+            <Nav.Link href="#home" className={styles.sideLink}>HOME</Nav.Link>
+            <Nav.Link href="#calendario" className={styles.sideLink}>CALENDARIO</Nav.Link>
+            <Nav.Link href="#rosa" className={styles.sideLink}>ROSA</Nav.Link>
+            <Nav.Link href="#chi-siamo" className={styles.sideLink}>CHI SIAMO</Nav.Link>
+          </Nav>
+        </div>
+        
 
       </Container>
     </Navbar>
